7ad78e301980c0195dbab3c8d5b46ee86164dd50
smyk-shop <rinatsarapov03@gmail.com>
2026-03-30 10:26:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// Этот файл хранится только у тебя на ПК
 const CONFIG = {
     TELEGRAM_BOT_TOKEN: '8587915362:AAEDNB3PchclVU7UxGKDUrQ3WbfvM0abXTw', 
     TELEGRAM_CHAT_ID: '1387608473'                   
